18f0d0a274be819608f7a536a80ac044e8d14a12
NikyS02 <schonova.nikola@gmail.com>
2022-11-02 09:59:40 +0100

    Týdenní správa gitu
    vytvoření složky pro analýzu 2.sprintu
    aktualizace týdenní tabulky
    záloha kódu projektu na git

---
+++ dokumentace/projekt_záloha_kodu/connect.php
@@ -0,0 +1,8 @@
+<?php                                                                                  
+$servername = "localhost";
+$username = "schonova";
+$password = "Tis*1819956";
+$dbname = "schonova";
+$spojeni = mysqli_connect($servername, $username, $password, $dbname);
+ mysqli_set_charset($spojeni, "utf8");
+?>
